9e129c4ed9a6c7038aaa507679a73dcf17f43f82
Drakmyth <shaun.hamman@gmail.com>
2024-05-30 11:50:40 -0700

    Wire up opening and closing Add IWAD modal

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,37 +1,21 @@
 import './style.css';
 import './app.css';
 
-import {Greet} from '../wailsjs/go/main/App';
-
-// Setup the greet function
-window.greet = function () {
-    // Get name
-    let name = nameElement!.value;
-
-    // Check if the input is empty
-    if (name === "") return;
-
-    // Call App.Greet(name)
-    try {
-        Greet(name)
-            .then((result) => {
-                // Update result with data back from App.Greet()
-                resultElement!.innerText = result;
-            })
-            .catch((err) => {
-                console.error(err);
-            });
-    } catch (err) {
-        console.error(err);
-    }
-};
-
-let nameElement = (document.getElementById("name") as HTMLInputElement);
-nameElement.focus();
-let resultElement = document.getElementById("result");
+// import {Greet} from '../wailsjs/go/main/App';
 
 declare global {
     interface Window {
-        greet: () => void;
+        openAddIWADModal: () => void;
+        closeAddIWADModal: () => void;
     }
+}
+
+window.openAddIWADModal = function () {
+    let openIWADModalCover = (document.getElementsByClassName("modal-cover")[0] as HTMLDivElement);
+    openIWADModalCover.classList.add("show-modal");
+}
+
+window.closeAddIWADModal = function () {
+    let openIWADModalCover = (document.getElementsByClassName("modal-cover")[0] as HTMLDivElement);
+    openIWADModalCover.classList.remove("show-modal");
 }
