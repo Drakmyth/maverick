327bfde5f928d498f444355339134df3ecaac477
Drakmyth <shaun.hamman@gmail.com>
2024-05-30 18:22:24 -0700

    Consolidate frontend navigate functions

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,44 +1,50 @@
-import './style.css';
-import './app.css';
+import "./style.css";
+import "./app.css";
 
-import {GetHomePage, GetEnginesPage, GetIWADsPage} from '../wailsjs/go/main/App';
+import {
+    GetHomePage,
+    GetEnginesPage,
+    GetIWADsPage,
+} from "../wailsjs/go/main/App";
 
 declare global {
     interface Window {
         openAddIWADModal: () => void;
         closeAddIWADModal: () => void;
-        navigateToHome: () => void;
-        navigateToEngines: () => void;
-        navigateToIWADs: () => void;
+        navigateTo: (page: string) => void;
         mavInit: () => void;
     }
 }
 
 window.openAddIWADModal = function () {
-    let openIWADModalCover = (document.getElementsByClassName("modal-cover")[0] as HTMLDivElement);
+    let openIWADModalCover = document.getElementsByClassName(
+        "modal-cover"
+    )[0] as HTMLDivElement;
     openIWADModalCover.classList.add("show-modal");
-}
+};
 
 window.closeAddIWADModal = function () {
-    let openIWADModalCover = (document.getElementsByClassName("modal-cover")[0] as HTMLDivElement);
+    let openIWADModalCover = document.getElementsByClassName(
+        "modal-cover"
+    )[0] as HTMLDivElement;
     openIWADModalCover.classList.remove("show-modal");
-}
+};
 
-window.navigateToHome = async function () {
+window.navigateTo = async function (page: string) {
     let appDiv = document.getElementById("app") as HTMLDivElement;
-    appDiv.innerHTML = await GetHomePage()
-}
-
-window.navigateToEngines = async function () {
-    let appDiv = document.getElementById("app") as HTMLDivElement;
-    appDiv.innerHTML = await GetEnginesPage()
-}
-
-window.navigateToIWADs = async function () {
-    let appDiv = document.getElementById("app") as HTMLDivElement;
-    appDiv.innerHTML = await GetIWADsPage()
-}
+    switch (page) {
+        case "home":
+            appDiv.innerHTML = await GetHomePage();
+            break;
+        case "engines":
+            appDiv.innerHTML = await GetEnginesPage();
+            break;
+        case "iwads":
+            appDiv.innerHTML = await GetIWADsPage();
+            break;
+    }
+};
 
 window.mavInit = function () {
-    window.navigateToHome()
-}
+    window.navigateTo("home");
+};
